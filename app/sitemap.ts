fea4c1385192bd7bdddb78aa338cf0264ac810df
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-02-08 13:37:56 +0200

    add https:// to sitemap link

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "www.قهوجىين.com";
+  const baseUrl = "https://www.قهوجىين.com";
 
   return [
     {
